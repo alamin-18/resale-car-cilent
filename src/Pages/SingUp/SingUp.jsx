05be4f5f24629a1c4223cb7341cdb9eb73e48201
Md Al Amin <fpalamin6@gmail.com>
2022-11-25 18:40:51 +0600

    user save database

--- src/Pages/SingUp/SingUp.jsx
+++ src/Pages/SingUp/SingUp.jsx
@@ -19,7 +19,7 @@ const SingUp = () => {
                 }
                 updateUser(userInfo)
                     .then(() => {
-                        // saveUser(data.name, data.email);
+                        saveUser(data.name, data.email);
                     })
                     .catch(err => console.log(err));
             })
@@ -28,6 +28,20 @@ const SingUp = () => {
                 toast.success(error.message)
                 // setSignUPError(error.message)
             });
+    }
+    const saveUser = (name, email) =>{
+        const user ={name, email};
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            // setCreatedUserEmail(email);
+        })
     }
     return (
         <div>
