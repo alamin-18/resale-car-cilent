c1009ed4bb6549dcb3216836826fffc92ef2cd3a
Md Al Amin <fpalamin6@gmail.com>
2022-11-25 15:12:51 +0600

    Catagory added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Main from './Layout/Main/Main';
 import Home from './Pages/Home/Home/Home';
+import LogIn from './Pages/LogIn/LogIn';
+import SingUp from './Pages/SingUp/SingUp';
 
 
 function App() {
   const routes = createBrowserRouter([
     {path:'/',element:<Main></Main>,children:[
-     {path:'/',element:<Home></Home>}
+     {path:'/',element:<Home></Home>},
+     {path:'/login',element:<LogIn></LogIn>},
+     {path:'/singup',element:<SingUp></SingUp>},
     ]}
   ])
   return (
